cbb995b67b112f931887ac49b8031a75980b30b1
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-08-02 07:32:18 +0530

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -14,28 +14,28 @@ ANON = "https://telegra.ph/file/90552395a5e96d0e7fab9.jpg"
 async def repo(client, message):
     await message.reply_photo(
         photo=ANON,
-        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [ᴢᴇɴ ✘ ʀᴏʙᴏᴛ-🇮🇳](t.me/zenxrobot)**
+        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [Group manegement](t.me/lovely420_bot)**
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [𝐀𝐃𝐈𝐓𝐘𝐀](t.me/pythonxgamer)
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [꧁༒︎༒︎༆༆𝚂𝚒𝚟𝚊𝚝𝚑𝚎𝚔𝚒𝚗𝚐༆༆༒︎༒︎꧂](t.me/Sivatheking_1)
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**ᴢᴇɴ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ᴘʀɪᴠᴀᴛᴇ 🥺 sᴏʀʀʏ ʙᴜᴛ ᴜ ᴄᴀɴ ᴜsᴇ ᴢᴇɴ ʀᴏʙᴏᴛ ғᴏʀ ɢʀᴏᴜᴘs.**
+**Group management bot sᴏᴜʀᴄᴇ ɪs ᴘʀɪᴠᴀᴛᴇ 🥺 sᴏʀʀʏ ʙᴜᴛ ᴜ ᴄᴀɴ ᴜsᴇ Group manegement bot ғᴏʀ ɢʀᴏᴜᴘs.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("•👑 ᴏᴡɴᴇʀ ʟᴇɢᴇɴᴅ 👑•", url="https://t.me/pythonxgamer"
+                    InlineKeyboardButton("•👑 ᴏᴡɴᴇʀ ʟᴇɢᴇɴᴅ 👑•", url="https://t.me/Sivatheking_1"
                     ),
                     InlineKeyboardButton(
                         "•💚sᴜᴘᴘᴏʀᴛ💚•",
-                        url="https://t.me/zensupport"
+                        url="https://t.me/pyrogram_support"
                     ),
                 ],
                 [
-                    InlineKeyboardButton("• ➕ ᴀᴅᴅ ᴛᴏ ɢʀᴏᴜᴘ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ➕ •", url="https://t.me/ZenXrobot?startgroup=true"),     
+                    InlineKeyboardButton("• ➕ ᴀᴅᴅ ᴛᴏ ɢʀᴏᴜᴘ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ➕ •", url="https://t.me/Lovely420_bot?startgroup=true"),     
                 ],
             ]
         ),
